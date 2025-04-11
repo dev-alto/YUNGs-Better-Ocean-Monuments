ce5fcda46ad685826cbe0f3254e43b932de95e86
montmaj <timelord1102@gmail.com>
2025-04-10 21:56:48 -0400

    Update Fabric, Forge, Neoforge to 1.21.5

--- Common/src/main/java/com/yungnickyoung/minecraft/betteroceanmonuments/world/processor/LegProcessor.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betteroceanmonuments/world/processor/LegProcessor.java
@@ -42,10 +42,10 @@ public class LegProcessor extends StructureProcessor {
             BlockPos.MutableBlockPos mutable = blockInfoGlobal.pos().mutable().move(Direction.DOWN);
             BlockState currBlockState = levelReader.getBlockState(mutable);
 
-            while (mutable.getY() > levelReader.getMinBuildHeight()
-                    && mutable.getY() < levelReader.getMaxBuildHeight()
+            while (mutable.getY() > levelReader.getMinY()
+                    && mutable.getY() < levelReader.getMaxY()
                     && (currBlockState.isAir() || !levelReader.getFluidState(mutable).isEmpty())) {
-                levelReader.getChunk(mutable).setBlockState(mutable, Blocks.PRISMARINE_BRICKS.defaultBlockState(), false);
+                levelReader.getChunk(mutable).setBlockState(mutable, Blocks.PRISMARINE_BRICKS.defaultBlockState(), 1);
                 mutable.move(Direction.DOWN);
                 currBlockState = levelReader.getBlockState(mutable);
             }
--- Forge/src/main/java/com/yungnickyoung/minecraft/betteroceanmonuments/BetterOceanMonumentsForge.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/betteroceanmonuments/BetterOceanMonumentsForge.java
@@ -1,10 +1,13 @@
 package com.yungnickyoung.minecraft.betteroceanmonuments;
 
+import com.yungnickyoung.minecraft.betteroceanmonuments.module.ConfigModuleForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BetterOceanMonumentsCommon.MOD_ID)
 public class BetterOceanMonumentsForge {
-    public BetterOceanMonumentsForge() {
+    public BetterOceanMonumentsForge(FMLJavaModLoadingContext fmlJavaModLoadingContext) {
         BetterOceanMonumentsCommon.init();
+        ConfigModuleForge.init(fmlJavaModLoadingContext);
     }
 }
